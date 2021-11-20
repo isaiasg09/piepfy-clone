2e70229a370378acbf2043acfedcbfee5cfc5e36
Isaias <isaiashsobral@gmail.com>
2021-11-19 22:35:06 -0300

    Initial project files

---
+++ src/components/Board/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import List from '../List';
+
+import { Container } from './styles';
+
+export default function Board() {
+  return (
+    <Container>
+      
+      <List />
+      <List />
+      <List />
+      <List />
+
+
+    </Container>
+  )
+}
---
+++ src/components/Card/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import { Container, Label } from './styles'
+
+export default function Card() {
+  return (
+    <Container>
+      <header>
+        <Label color="#7159c1" />
+        
+      </header>
+      <p>Fazer a migração completa do server</p>
+      <img src="https://avatars.githubusercontent.com/isaiasg09" alt="" />
+    </Container>
+  )
+}
