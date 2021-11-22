c303ff4a77fdba9af0d936c9f67217532388e302
Isaias <isaiashsobral@gmail.com>
2021-11-22 14:51:35 -0300

    Save theme to local storage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
+import usePersistedState from "./utils/usePersistedState";
 
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from './styles/global';
@@ -12,7 +13,7 @@ import Header from "./components/Header";
 import Board from "./components/Board";
 
 function App() {
-  const [theme, setTheme] = useState(light);
+  const [theme, setTheme] = usePersistedState('theme', light);
 
   const toggleTheme = ()=>{
     setTheme(theme.title === 'light' ? dark : light);
